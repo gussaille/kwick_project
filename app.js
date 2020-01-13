815fa90af3a9a94712484cf448b7d156ef9d6703
Nicolas Durocher <ndurocher@hotmail.fr>
2020-01-13 14:49:40 +0100

    Add signup/login functionalities

--- app.js
+++ app.js
@@ -1,3 +1,71 @@
 $(function(){
-  
+    const URL_API = "http://greenvelvet.alwaysdata.net/kwick/api/";
+
+    function getApiPing(){
+        $.ajax({
+            url: `${URL_API}ping`,
+            method: "GET",
+            dataType : "jsonp",
+        })
+        .done(function(response){
+            let data = JSON.stringify(response);
+            console.log(data);
+        })
+        .fail(function(error){
+            console.log("Echec" + JSON.stringify(error));
+        })
+    }
+    // getApiPing();
+
+    function signUp(evt){
+        evt.preventDefault();
+
+        let signUpUser = $('#username-signUp').val(),
+            signUpPw = $('#password-signUp').val();
+        
+        const SIGNUP_API = `${URL_API}/signup/${signUpUser}/${signUpPw}`;
+        console.log(SIGNUP_API);
+
+        $.ajax({
+            url: SIGNUP_API,
+            method: "GET",
+            dataType : "jsonp",
+        })
+        .done(function(response){
+            console.log("Inscription réussie")
+            let data = JSON.stringify(response);
+            console.log(data);
+        })
+        .fail(function(error){
+            console.log("Echec" + JSON.stringify(error));
+        });
+    }
+
+    function logIn(evt){
+        evt.preventDefault();
+
+        let loginUser = $('#username-login').val(),
+            loginPw = $('#password-login').val();
+        
+        const LOGIN_API = `${URL_API}/login/${loginUser}/${loginPw}`;
+        console.log(LOGIN_API);
+    
+        $.ajax({
+            url: LOGIN_API,
+            method: "GET",
+            dataType : "jsonp",
+        })
+        .done(function(response){
+            console.log("Connexion réussie")
+            let data = JSON.stringify(response);
+            console.log(data);
+        })
+        .fail(function(error){
+            console.log("Echec" + JSON.stringify(error));
+        });
+    }
+
+    $('.form-signup').on('submit', signUp);
+    $('.form-login').on('submit', logIn);
+
 });
